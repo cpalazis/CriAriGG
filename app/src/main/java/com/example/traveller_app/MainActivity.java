9f5e7dc47b7d5271381b2d553374ec370b2a1be1
cpalazis <cpalazis@yahoo.com>
2022-11-17 18:52:50 +0200

    SightsActivity for Kappy_Traveller

--- app/src/main/java/com/example/traveller_app/MainActivity.java
+++ app/src/main/java/com/example/traveller_app/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.traveller_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView Hello;
+    private Button Museums;
+    private Button Hotels;
+    private Button Banks;
+    private Button Caffe;
+    private Button Photos;
+    private Button Restaurants;
+    private Button Sights;
+    private Button General_Informations;
+
 
 
     @SuppressLint("MissingInflatedId")
@@ -15,7 +26,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Hello=findViewById(R.id.Text_Hello);
-        Hello.setText("Hello Criss !");
+        Museums=findViewById(R.id.button);
+        Hotels =findViewById(R.id.button2);
+        Banks=findViewById(R.id.button3);
+        Caffe=findViewById(R.id.button4);
+        Photos=findViewById(R.id.button5);
+        Restaurants=findViewById(R.id.button6);
+        Sights=findViewById(R.id.button7);
+        General_Informations=findViewById(R.id.button8);
+
+
+        Sights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent New_Screen=new Intent(getApplicationContext(),SightsActivity.class);
+                startActivity(New_Screen);
+            }
+        });
+
     }
 }
